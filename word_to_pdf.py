0e9e4a976bb2c2d82d7d9e6255f2192a0a961836
Syed Huzaifa <huzaifanau123@gmail.com>
2021-06-28 23:46:34 +0500

    Created Main Window

---
+++ word_to_pdf.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter.filedialog import askopenfile
+from tkinter.messagebox import showinfo
+from docx2pdf import convert
+
+win = tk.Tk()
+win.title("Word To PDF Converter Tool")
+
